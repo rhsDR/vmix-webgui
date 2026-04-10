87ec779180c473b3e8765ec73037f9259d022aa0
rhsDR <rhs@dr.dk>
2026-04-10 12:11:59 +0200

    nu med gul rød udvisning data til json

--- api/vmix/[id].js
+++ api/vmix/[id].js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
 
     const [projektRaw, kampeRaw, subsRaw, vmixCallsRaw, tickersRaw, settingsRaw] = await Promise.all([
       sbGet('projekter?id=eq.' + pid + '&select=navn,type,undertitel&limit=1'),
-      sbGet('kampe?projekt_id=eq.' + pid + '&select=slot,hold1_lang,hold1_kort,hold1_score,hold2_score,hold2_kort,hold2_lang,kommentator,lokation,vmixcall,on_air&order=slot.asc'),
+      sbGet('kampe?projekt_id=eq.' + pid + '&select=slot,hold1_lang,hold1_kort,hold1_score,hold2_score,hold2_kort,hold2_lang,kommentator,lokation,vmixcall,on_air,last_card_type,last_card_player,last_card_min,last_card_team_kort&order=slot.asc'),
       sbGet('subs?projekt_id=eq.' + pid + '&select=slot,navn,titel&order=slot.asc'),
       sbGet('vmix_calls?projekt_id=eq.' + pid + '&select=slot,navn,titel,link&order=slot.asc'),
       sbGet('tickers?projekt_id=eq.' + pid + '&select=slot,overskrift,tekst,on_air,breaking&order=slot.asc'),
@@ -102,8 +102,12 @@ export default async function handler(req, res) {
           json[`K${s}_kom`]   = r.kommentator || '';
           json[`K${s}_lok`]   = r.lokation    || '';
           json[`K${s}_vmc`]   = r.vmixcall    || '';
-          json[`K${s}_oA`]    = r.on_air      || false;
-          json[`K${s}_samf`]  = h1 && h2 ? `${h1} ${r.hold1_score || 0} - ${r.hold2_score || 0} ${h2}` : '';
+          json[`K${s}_oA`]       = r.on_air             || false;
+          json[`K${s}_samf`]     = h1 && h2 ? `${h1} ${r.hold1_score || 0} - ${r.hold2_score || 0} ${h2}` : '';
+          json[`K${s}_card_t`]   = r.last_card_type      || '';
+          json[`K${s}_card_p`]   = r.last_card_player     || '';
+          json[`K${s}_card_min`] = r.last_card_min        || '';
+          json[`K${s}_card_tm`]  = r.last_card_team_kort  || '';
         });
     }
 
